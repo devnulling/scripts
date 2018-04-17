8dfffeca47905d64926ca0168d5c37127cf547ef
devnulling <devnulling@users.noreply.github.com>
2018-04-16 18:59:01 -0700

    rm spaces

--- sdr.sh
+++ sdr.sh
@@ -3,12 +3,10 @@
 SDR_BASE_DIR="/home/$USER/sdr"
 SDR_INSTALL_TARGET="$SDR_BASE_DIR/installs"
 
-
 # pick a UHD tag
 #SDR_UHD_VERSION="release_003_009_002"
 #SDR_UHD_VERSION="release_003_009_006"
 #SDR_UHD_VERSION="release_003_009_007"
-
 #SDR_UHD_VERSION="release_003_010_000_000"
 #SDR_UHD_VERSION="release_003_010_001_000"
 #SDR_UHD_VERSION="release_003_010_002_000"
@@ -26,26 +24,16 @@ SDR_GR_VERSION="v3.7.12.0"
 SDR_INSTALL_UID="$SDR_UHD_VERSION"
 
 # setup paths where stuff goes
-
-
 SDR_INSTALL_BASE="$SDR_INSTALL_TARGET/$SDR_INSTALL_UID"
-
 SDR_SRC_TARGET="$SDR_INSTALL_BASE/src"
-
 SDR_UHD_SRC_BASE="$SDR_SRC_TARGET/uhd"
 SDR_GR_SRC_BASE="$SDR_SRC_TARGET/gnuradio"
-
-
-
-
-
 
 SDR_ENV_FILE="$SDR_INSTALL_BASE/setup.env"
 SDR_OOT_DIR="$SDR_INSTALL_BASE/oots"
 SDR_OOT_BUILDER_FILE="$SDR_OOT_DIR/oot.sh"
 SDR_UHD_CMAKE="cmake -DCMAKE_INSTALL_PREFIX=$SDR_INSTALL_BASE ../"
 SDR_GR_CMAKE="cmake -DCMAKE_INSTALL_PREFIX=$SDR_INSTALL_BASE -DUHD_DIR=$SDR_INSTALL_BASE/lib/cmake/uhd/ -DUHD_INCLUDE_DIRS=$SDR_INSTALL_BASE/include/ -DUHD_LIBRARIES=$SDR_INSTALL_BASE/lib/libuhd.so ../"
-
 
 check_dir(){
 	CURRENT_DIR=`pwd`
@@ -68,7 +56,6 @@ install_uhd(){
 
 	git clone https://github.com/ettusresearch/uhd $SDR_UHD_SRC_BASE
 	
-	
 	cd "$SDR_UHD_SRC_BASE/"
 	check_dir
 
@@ -79,19 +66,13 @@ install_uhd(){
 
 	mkdir -p "$SDR_UHD_SRC_BASE/host/build"
 	cd "$SDR_UHD_SRC_BASE/host/build"
-
 	check_dir
-
+	
 	eval $SDR_UHD_CMAKE
-	
 	make -j7
-	
 	make install
-
 	source $SDR_ENV_FILE
-	
 	uhd_usrp_probe --version
-	
 	uhd_images_downloader
 	
 }
@@ -153,7 +134,6 @@ write_oot_builder(){
 	echo "make install" >> $SDR_OOT_BUILDER_FILE
 	chmod +x $SDR_OOT_BUILDER_FILE
 }
-
 
 create_dirs
 write_env_file
